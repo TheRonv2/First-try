a79c0e954f139b342d9a51611ec1fd6740188ef5
TheRonv2 <boss.0057@bk.ru>
2023-11-14 22:21:07 +0300

    First commit

---
+++ Assets/Scenes/Scripts/EnemyAttack.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject target;
+    public float attackTimer;
+    public float coolDown;
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        attackTimer = 0;
+        coolDown = 2.0f;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTimer > 0)
+            attackTimer -= Time.deltaTime;
+
+        if (attackTimer < 0)
+            attackTimer = 0;
+
+        if (attackTimer == 0)
+        {
+            Attack();
+            attackTimer = coolDown;
+        }
+    }
+
+    private void Attack()
+    {
+        
+        {
+            float direction = player.transform.position.x - transform.position.x;
+
+            if (Mathf.Abs(direction) < 20)
+            {
+                PlayersHealth eh = (PlayersHealth)target.GetComponent("PlayersHealth");
+                eh.AddjustCurrentHealth(-10);
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/PlayerAttack.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject target;
+    public float attackTimer;
+    public float coolDown;
+    GameObject Enemy;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        attackTimer = 0;
+        coolDown = 2.0f;
+        Enemy = GameObject.FindWithTag("Enemy");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTimer > 0)
+            attackTimer -= Time.deltaTime;
+
+        if (attackTimer < 0)
+            attackTimer = 0;
+
+        if (Input.GetKeyUp(KeyCode.F) && (Input.GetAxis("Horizontal") == 0))
+        {
+            if (attackTimer == 0)
+            {
+                anim.SetTrigger("attack");
+                Attack();
+                attackTimer = coolDown;
+            }
+        }
+        
+
+    }
+
+    private void Attack()
+    {
+        {
+            float direction = Enemy.transform.position.x - transform.position.x;
+
+            if (Mathf.Abs(direction) < 20)
+            {
+                EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
+                eh.AddjustCurrentHealth(-10);
+            }
+        }
+    }
+}
